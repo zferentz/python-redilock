1420a3c8c2d6b3d37910af84299ada3564a477f0
Zvika Ferentz <zferentz@rapidsos.com>
2024-09-27 03:45:38 +0000

    Create better directory structure

--- src/async_redilock.py
+++ src/async_redilock.py
@@ -5,38 +5,21 @@ import hashlib
 import uuid
 import time
 
-_redis: aioredis.Redis = None
-
-
-_REDIS_HOST = None
-_REDIS_POST = None
-_REDIS_DB = None
-
-async def initialize():
-    global _redis
-    _redis = await aioredis.from_url(
-        url=f"redis://{_REDIS_HOST}:{REDIS_PORT}",
-        db=_REDIS_DB,
-        password=None,
-        encoding="utf-8",
-        decode_responses=True
-    )
-
-    await _redis.ping()
-
 
 class _RediScript:
     def __init__(self, script: str):
         self._script = script
-        self._script_sha1 = hashlib.sha1(script.encode("utf-8")).hexdigest()  # nosec
+        self._script_sha1 = hashlib.sha1(script.encode("utf-8")).hexdigest()
 
-    async def run(self, keys: tuple, args: list):
+    async def run(self, _redis: aioredis.Redis, keys: tuple, args: list):
         try:
-            result = await _redis.evalsha(self._script_sha1, len(keys), *keys, *args)
+            result = await _redis.evalsha(self._script_sha1, len(keys), *keys,
+                                          *args)
 
         except aioredis.exceptions.NoScriptError:
             await _redis.script_load(self._script)
-            result = await _redis.evalsha(self._script_sha1, len(keys), *keys, *args)
+            result = await _redis.evalsha(self._script_sha1, len(keys), *keys,
+                                          *args)
 
         return result
 
@@ -56,12 +39,29 @@ _UNLOCK_SCRIPT = _RediScript("""
 class DistributedLock:
     """Distributed Lock"""
 
-    @classmethod
+    def __init__(self, redis_host: str, redis_port: int, redis_db: int):
+        self._redis: aioredis.Redis = None
+        self._redis_host: str = redis_host
+        self._redis_port: int = redis_port
+        self._redis_db: int = redis_db
+
+    async def _connect(self):
+
+        self._redis = await aioredis.from_url(
+            url=f"redis://{self._redis_host}:{self._redis_port}/{self}",
+            db=self._redis_db,
+            password=None,
+            encoding="utf-8",
+            decode_responses=True
+        )
+
+        await self._redis.ping()
+
     async def lock(
-            cls,
+            self,
             lock_name: str,
             ttl: float,
-            max_wait: float = None,
+            block: float | bool = True,
             interval: float = 0.25
     ):
         """Lock a resource (by lock name).  Wait until lock is owned.
@@ -87,25 +87,31 @@ class DistributedLock:
           system error the lock will be auto-released.
         """
 
+        if not self._redis:
+            await self._connect()
+
+        assert isinstance(ttl, (int, float)) and ttl > 0, "ttl must be >0"
         assert isinstance(interval, (int, float)) and interval > 0, \
             "interval must be >0"
-        if max_wait is not None:
-            assert isinstance(max_wait, (int, float)) and max_wait > 0, \
+        if block not in (True, False):
+            assert isinstance(block, (int, float)) and block > 0, \
                 "max_wait must be >0"
-            end_wait = time.time() + max_wait
+            end_wait = time.time() + block
         else:
             end_wait = None
 
+        unlock_secret = f"_LOCK_{lock_name}_{uuid.uuid4().hex}"
+
         while True:
             # Try to acquire lock
-            unlock_secret = await cls.try_lock(lock_name, ttl)
-
-            # If we got it - return the "secret" for unlock
-            if unlock_secret:
+            ret = await self._redis.set(
+                lock_name, unlock_secret, px=int(ttl * 1000), nx=True
+            )
+            if ret:
                 return unlock_secret
 
             # If did not acquire lock - check if max_time has been exceeded
-            if end_wait and time.time() >= end_wait:
+            if not block or (end_wait and time.time() >= end_wait):
                 return False
 
             # Sleep before retrying to obtain the lock again
@@ -113,27 +119,23 @@ class DistributedLock:
 
         assert False, "Can never be here"
 
-    @classmethod
-    async def try_lock(cls, lock_name: str, ttl: float):
-        """Lock a resource (by lock name).  return False if lock wasn't acquired
-
-        :param str lock_name: the name of the lock  (e.g. resource to lock)
-        :param float|int ttl: Time To Leave (or Time To Lock) in seconds
-
-        :returns: string (token to unlock) or False for failure (couldn't lock)
-        """
-        assert isinstance(ttl, (int, float)) and ttl > 0, "ttl must be >0"
-        unlock_secret = f"_LOCK_{lock_name}_{uuid.uuid4().hex}"
-        ret = await _redis.set(
-            lock_name, unlock_secret, px=int(ttl * 1000), nx=True
-        )
-        assert ret in (True, None)
-        if not ret:
-            return False
-        return unlock_secret
-
-    @classmethod
-    async def unlock(cls, lock_name: str, unlock_secret: str) -> bool:
+    async def unlock(self, lock_name: str, unlock_secret: str) -> bool:
+        if not self._redis:
+            await self._connect()
 
         # Run the unlock-script with lock_name as key and unlock_secret as argv
-        return await _UNLOCK_SCRIPT.run((lock_name,), [unlock_secret, ]) == 1
+        return 1 == await _UNLOCK_SCRIPT.run(
+            self._redis, (lock_name,), [unlock_secret, ]
+        )
+
+async def main():
+    dl = DistributedLock("127.0.0.1", 6379, 5)
+    lock = await dl.lock("zvika1", 5)
+    print(lock)
+    await dl.unlock("zvika1", lock)
+    print(await dl.lock("zvika1", 5, block=False))
+    lock = await dl.lock("zvika1", 5)
+    print(lock)
+
+
+asyncio.run(main())
--- src/redilock.py
+++ src/redilock.py
@@ -4,21 +4,15 @@ import hashlib
 import uuid
 import time
 
-_redis = None
-def initialize(redis_host: str, redis_port: int, redis_db: int)
-    """Initialize redis"""
-    global _redis
-    _redis = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
-    _redis.ping()
-  
+
 class _RediScript:
-    """Helper - an easy way to run redis Lua script
+    """Helper - an easy way to run redis Lua script"""
 
     def __init__(self, script: str):
         self._script = script
         self._script_sha1 = hashlib.sha1(script.encode("utf-8")).hexdigest()
 
-    def run(self, keys: tuple, args: list):
+    def run(self, _redis, keys: tuple, args: list):
         """Run the Lua script with parameters  (keys and args)"""
         try:
             result = _redis.evalsha(self._script_sha1, len(keys), *keys, *args)
@@ -43,12 +37,23 @@ _UNLOCK_SCRIPT = _RediScript("""
 class DistributedLock:
     """Distributed Lock"""
 
-    @classmethod
+    def __init__(self, redis_host: str, redis_port: int, redis_db: int):
+        self._redis: redis.Redis = None
+        self._redis_host: str = redis_host
+        self._redis_port: int = redis_port
+        self._redis_db: int = redis_db
+
+    def _connect(self):
+        self._redis = redis.StrictRedis(
+            host=self._redis_host, port=self._redis_port, db=self._redis_db
+        )
+        self._redis.ping()
+
     def lock(
-            cls,
+            self,
             lock_name: str,
             ttl: float,
-            max_wait: float = None,
+            block: float | bool = True,
             interval: float = 0.25
     ):
         """Lock a resource (by lock name).  Wait until lock is owned.
@@ -74,25 +79,31 @@ class DistributedLock:
           system error the lock will be auto-released.
         """
 
+        if not self._redis:
+            self._connect()
+
+        assert isinstance(ttl, (int, float)) and ttl > 0, "ttl must be >0"
         assert isinstance(interval, (int, float)) and interval > 0, \
             "interval must be >0"
-        if max_wait is not None:
-            assert isinstance(max_wait, (int, float)) and max_wait > 0, \
+        if block not in (True, False):
+            assert isinstance(block, (int, float)) and block > 0, \
                 "max_wait must be >0"
-            end_wait = time.time() + max_wait
+            end_wait = time.time() + block
         else:
             end_wait = None
 
+        unlock_secret = f"_LOCK_{lock_name}_{uuid.uuid4().hex}"
+
         while True:
             # Try to acquire lock
-            unlock_secret = cls.try_lock(lock_name, ttl)
-
-            # If we got it - return the "secret" for unlock
-            if unlock_secret:
+            ret = self._redis.set(
+                lock_name, unlock_secret, px=int(ttl * 1000), nx=True
+            )
+            if ret:
                 return unlock_secret
 
             # If did not acquire lock - check if max_time has been exceeded
-            if end_wait and time.time() >= end_wait:
+            if not block or (end_wait and time.time() >= end_wait):
                 return False
 
             # Sleep before retrying to obtain the lock again
@@ -100,25 +111,20 @@ class DistributedLock:
 
         assert False, "Can never be here"
 
-    @classmethod
-    def try_lock(cls, lock_name: str, ttl: float):
-        """Lock a resource (by lock name).  return False if lock wasn't acquired
-
-        :param str lock_name: the name of the lock  (e.g. resource to lock)
-        :param float|int ttl: Time To Leave (or Time To Lock) in seconds
-
-        :returns: string (token to unlock) or False for failure (couldn't lock)
-        """
-        assert isinstance(ttl, (int, float)) and ttl > 0, "ttl must be >0"
-        unlock_secret = f"_LOCK_{lock_name}_{uuid.uuid4().hex}"
-        ret = _redis.set(lock_name, unlock_secret, px=int(ttl * 1000), nx=True)
-        assert ret in (True, None)
-        if not ret:
-            return False
-        return unlock_secret
-
-    @classmethod
-    def unlock(cls, lock_name: str, unlock_secret: str) -> bool:
+    def unlock(self, lock_name: str, unlock_secret: str) -> bool:
+        if not self._redis:
+            self._connect()
 
         # Run the unlock-script with lock_name as key and unlock_secret as argv
-        return _UNLOCK_SCRIPT.run((lock_name,), [unlock_secret, ]) == 1
+        return 1 == _UNLOCK_SCRIPT.run(
+            self._redis, (lock_name,), [unlock_secret, ]
+        )
+
+
+dl = DistributedLock("127.0.0.1", 6379, 5)
+lock = dl.lock("zvika1", 5)
+print(lock)
+#dl.unlock("zvika1", lock)
+print(dl.lock("zvika1", 5, block=False))
+lock = dl.lock("zvika1", 5)
+print(lock)
